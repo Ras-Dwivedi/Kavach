e568447571238b059dbc2e23da7f366ae342889a
Ankush Desai <ankushdesai@gmail.com>
2018-10-01 13:32:04 -0700

    instrumented the BallotBox.scala code

--- src/main/scala/VotingSystem/BallotBox.scala
+++ src/main/scala/VotingSystem/BallotBox.scala
@@ -3,10 +3,14 @@ import org.apache.spark.sql
 import org.apache.spark.sql.Dataset
 
 class BallotBox (bb: Dataset[Vote]) extends java.io.Serializable {
+  var policy = new BallotBoxPolicy(BB_VotingState("Voting"))
+
   val ballotBox : Dataset[Vote] = bb
   case class Voter(voterId: Int)
 
   def addVote(vote: Vote) : Any = {
+      policy.transition(CastVote("addVote"))
+
       print(ballotBox.getClass())
       import Session.spark.sqlContext.implicits._
       val ds = Seq(Vote(vote.readCredential(), vote.readVoteId(), vote.readVote())).toDS()
@@ -16,6 +20,8 @@ class BallotBox (bb: Dataset[Vote]) extends java.io.Serializable {
    }
 
   def removeDuplicates() : Any = {
+    policy.transition(RemoveDuplicate("removeDuplicates"))
+
     import org.apache.spark.sql.functions._
     import Session.spark.implicits._
     val ballotBox2 = ballotBox.map(v => (v.readCredential(), v)).groupByKey(_._1).reduceGroups((a, b) => (a._1, b._2)).map(_._2)
@@ -25,6 +31,8 @@ class BallotBox (bb: Dataset[Vote]) extends java.io.Serializable {
   }
 
   def generateResults(): Any = {
+    policy.transition(CountVotes("generateResults"))
+
     import Session.spark.implicits._
     val vote_counts = ballotBox.map(v => (v.readVote(), 1)).groupByKey(_._1).reduceGroups((a, b) => (a._1, a._2 + b._2)).map(_._2)
     vote_counts.show()
@@ -32,6 +40,8 @@ class BallotBox (bb: Dataset[Vote]) extends java.io.Serializable {
   }
 
   def checkVote(id: Int): Any = {
+    policy.transition(CheckVote("checkVote"))
+
     import Session.spark.implicits._
     val filtered_ballotbox = ballotBox.filter(v => v.readCredential() == id)
     filtered_ballotbox.show()
