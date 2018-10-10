782976f4cae098a854273a4274afac86d2058d58
Shivendra Kushwah <shivendrakushwah@gmail.com>
2018-10-10 16:30:48 -0700

    added more classes

---
+++ src/main/scala/HealthCareSystem/ICD10Diagnosis.scala
@@ -0,0 +1,15 @@
+/**
+  * Represents a ICD10Diagnosis object associated with a particular ICD10 code
+  * 
+  */
+case class ICD10Diagnosis(ICD10Code: Int, diagnosisDescription: String) {
+
+  def readICD10Code(): Int = {
+    return ICD10Code
+  }
+
+  def readDiagnosisDescription(): String = {
+    return diagnosisDescription
+  }
+  
+}
